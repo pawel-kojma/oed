efef0683c53a8c5136627d9184ef58155992c0f9
pawel-kojma <pawel.kojma@hotmail.com>
2024-01-12 15:16:15 +0100

    revert tsdl

--- bin/main.ml
+++ bin/main.ml
@@ -1,22 +1,3 @@
-open Tsdl
-
-let main () =
-  match Sdl.init Sdl.Init.(video + events) with
-  | Error (`Msg e) ->
-      Sdl.log "Init error: %s" e;
-      exit 1
-  | Ok () -> (
-      match Sdl.create_window ~w:640 ~h:480 "SDL OpenGL" Sdl.Window.opengl with
-      | Error (`Msg e) ->
-          Sdl.log "Create window error: %s" e;
-          exit 1
-      | Ok w ->
-          let s = Sdl.get_platform () in
-          print_endline s;
-          Sdl.pump_events ();
-          Sdl.delay 3000l;
-          Sdl.destroy_window w;
-          Sdl.quit ();
-          exit 0)
+let main () = print_endline "hello, world"
 
 let () = main ()
