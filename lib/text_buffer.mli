894eb620bcd83c33686a6e7381cce33590f47c95
pawel-kojma <pawel.kojma@hotmail.com>
2024-01-19 19:55:13 +0100

    add new required functions to text data structure api

--- lib/text_buffer.mli
+++ lib/text_buffer.mli
@@ -1,19 +1,38 @@
 module type TextEditDataStructure = sig
   type 'a t
 
+  (* create new instance *)
   val empty : 'a t
+
+  (* predicates *)
+  val is_empty : 'a t -> bool
+  val is_begin : 'a t -> bool
+  val is_end : 'a t -> bool
+
+  (* build from or decompose to list *)
   val of_list : 'a list -> 'a t
   val to_list : 'a t -> 'a list
+
+  (* move cursor around *)
   val move_left : 'a t -> 'a t
+  val move_left_n 'a t -> int -> 'a t
   val move_right : 'a t -> 'a t
+  val move_right_n 'a t -> int -> 'a t
+
+  (* insert element BEFORE the cursor *)
   val insert : 'a -> 'a t -> 'a t
+
+  (* remove element BEFORE the cursor *)
   val remove : 'a t -> 'a t
+
+  (* check element at cursor location *)
   val elem : 'a t -> 'a option
 end
 
 module type S = sig
     type container
     
+    val new : container
     val build : string -> container
     val decompose : container -> string
     val insert : char -> container -> container
