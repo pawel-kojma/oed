c7780c2138c7ff4ebbfd349c9e9170cd404f7525
pawel-kojma <pawel.kojma@hotmail.com>
2024-01-21 18:40:43 +0100

    del comments

--- bin/main.ml
+++ bin/main.ml
@@ -35,38 +35,3 @@ let () =
   in
   let r = EditorSt.run state (main_loop ()) in
   r
-
-(*
-let rec main_loop (ed : Mode.editor) (w : Curses.window) =
-  let key = Curses.getch () |> Key.convert in
-  match ed.edit_mode with
-  | Normal ->
-      ed.normal_action ed w key;
-      main_loop ed w;
-      ()
-  | Insert ->
-      ed.insert_action ed w key;
-      main_loop ed w;
-      ()
-
-let () =
-  Curses.use_env true;
-  let w = Curses.initscr () in
-  assert (Curses.cbreak ());
-  assert (Curses.noecho ());
-  assert (Curses.intrflush w false);
-  assert (Curses.keypad w true);
-  assert (Curses.wmove w 0 0);
-  Curses.scrollok w true;
-  let ed : Mode.editor =
-    {
-      edit_mode = Mode.Insert;
-      insert_action = Mode.insert_mode_action;
-      normal_action = Mode.normal_mode_action;
-    }
-  in
-  main_loop ed w;
-  Curses.endwin ();
-  ()
-
-*)
