c3d7c20061ac832b071c6483b25c71424112605f
pawel-kojma <pawel.kojma@hotmail.com>
2024-02-05 16:27:51 +0100

    create specified file

--- lib/file.ml
+++ lib/file.ml
@@ -1,4 +1,7 @@
-let read_file fname = In_channel.with_open_text fname In_channel.input_all
+let read_file fname =
+  In_channel.with_open_gen
+    [ In_channel.Open_creat; In_channel.Open_text ]
+    0o644 fname In_channel.input_all
 
 let write_file fname s =
   Out_channel.with_open_text fname (fun oc -> Out_channel.output_string oc s)
